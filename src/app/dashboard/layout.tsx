6256648bf48a465c3acd230a74e844eb8ee554ce
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-11-22 12:54:44 +0600

    fix: added dashboard protection from layout side also

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,14 @@
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 import { DashboardNav } from '../../feature/dashboard/dashboard-nav'
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const session = await auth.api.getSession({ headers: await headers() })
+  if (!session?.user) {
+    return redirect('/sign-in')
+  }
+
   return (
     <>
       <DashboardNav />
